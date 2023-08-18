e08034382357239d77007fb96765ce541e676670
senuravihanjayadeva <senurajayadeva@gmail.com>
2023-08-18 22:39:55 +0530

    Audio Only feature

--- src/app/audio/page.tsx
+++ src/app/audio/page.tsx
@@ -1,17 +1,15 @@
 "use client"
 import Image from "next/image";
 import React,{useRef} from "react";
-import VideoPlayer from "../components/VideoPlayer"
+import VideoPlayer from "../../components/VideoPlayer"
 
-export default function Home() {
+export default function Audio() {
   const playerRef = useRef(null);
 
   const videoJsOptions = {
+    audioOnlyMode: true,
     autoplay: true,
     controls: true,
-    responsive: true,
-    // poster: SampleImage,
-    language: "pt", // Set the language to Spanish
     playbackRates: [0.5, 1, 1.5, 2, 2.5, 3],
     controlBar: {
       skipButtons: {
@@ -25,20 +23,6 @@ export default function Home() {
         src: "https://firebasestorage.googleapis.com/v0/b/sri-lanka-airlines-storage.appspot.com/o/sample360.mp4?alt=media&token=7e4fb99e-0205-4042-90ff-080c0d091c42",
         type: "video/mp4",
         quality: "360p",
-        chapters: [
-          { label: "Chapter 1", time: "0" },
-          { label: "Chapter 2", time: "20" },
-          { label: "Chapter 3", time: "40" },
-          { label: "Chapter 4", time: "60" },
-          { label: "Chapter 5", time: "90" },
-          { label: "Chapter 6", time: "110" },
-          { label: "Chapter 7", time: "140" },
-        ],
-      },
-      {
-        src: "https://firebasestorage.googleapis.com/v0/b/sri-lanka-airlines-storage.appspot.com/o/sample720.mp4?alt=media&token=50b250ab-3845-4321-94d1-49987a4d706b",
-        type: "video/mp4",
-        quality: "720p",
         chapters: [
           { label: "Chapter 1", time: "0" },
           { label: "Chapter 2", time: "20" },
@@ -67,7 +51,7 @@ export default function Home() {
 
   return (
     <div style={{ padding: "5% 10%" }}>
-      <h1>Demo App - CloudFlicks</h1>
+      <h3>Demo App - CloudFlicks</h3>
       <hr />
       <VideoPlayer options={videoJsOptions} onReady={handlePlayerReady} />
     </div>
